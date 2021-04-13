b7feb2eb7398cdb463190b61f59de38d36bfe0e0
GaelMe <gmeanti@gmail.com>
2021-04-13 10:39:07 +0200

    Exo 15 blocage

--- exo_17.rb
+++ exo_17.rb
@@ -1,6 +1,12 @@
 puts "Donne un nombre"
 print "> "
 nombre = gets.chomp.to_i
+if nombre < 1
+    nombre = 1
+  elsif 
+    nombre > 25 
+    nombre = 25 
+  end
 nombre.times do |i|
       print ' ' * (nombre - i)
       puts '*' * (2 * i + 1)
